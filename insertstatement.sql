79faf77087a5e00a7081b3c34973041228644317
teamfbls <thomashartmann1@live.de>
2015-12-20 20:56:36 +0100

    Create insertstatement.sql

---
+++ insertstatement.sql
@@ -0,0 +1,146 @@
+use autover;
+-- ------------------Hersteller--------------
+insert into Hersteller(Hersteller_Name)
+values("BMW");
+insert into Hersteller(Hersteller_Name)
+values("Audi"); 
+insert into Hersteller(Hersteller_Name)
+values("Fiat"); 
+insert into Hersteller(Hersteller_Name)
+values("Ford"); 
+insert into Hersteller(Hersteller_Name)
+values("Honda"); 
+insert into Hersteller(Hersteller_Name)
+values("Mercedes"); 
+insert into Hersteller(Hersteller_Name)
+values("Opel"); 
+insert into Hersteller(Hersteller_Name)
+values("VW"); 
+insert into Hersteller(Hersteller_Name)
+values("Crysler"); 
+insert into Hersteller(Hersteller_Name)
+values("Porsche");  
+insert into Hersteller(Hersteller_Name)
+values("Skoda"); 
+insert into Hersteller(Hersteller_Name)
+values("Volvo");  
+-- ------------------Farbe------------------------
+insert into Farbe(Farbe)
+values("Schwarz");  
+insert into Farbe(Farbe)
+values("Rot");  
+insert into Farbe(Farbe)
+values("Blau");  
+insert into Farbe(Farbe)
+values("Gelb");
+insert into Farbe(Farbe)
+values("Weiß");  
+insert into Farbe(Farbe)
+values("Grün");  
+insert into Farbe(Farbe)
+values("Grau");  
+insert into Farbe(Farbe)
+values("Silber");  
+-- -----------------------Tueren-----------
+insert into tueren(Tuerenanzahl)
+values("2/3");  
+insert into tueren(Tuerenanzahl)
+values("4/5");  
+insert into tueren(Tuerenanzahl)
+values("6/7");  
+-- ---------------Kraftstoff
+insert into kraftstoff(kraftstofftyp)
+values("Diesel"); 
+insert into kraftstoff(kraftstofftyp)
+values("Benzin");  
+insert into kraftstoff(kraftstofftyp)
+values("Autogas");  
+insert into kraftstoff(kraftstofftyp)
+values("Hybrid"); 
+-- -------------Zustand
+
+insert into Zustand(zustand)
+values("Neuwagen"); 
+insert into Zustand(zustand)
+values("Gebraucht");   
+insert into Zustand(zustand)
+values("Tageszulassung");  
+
+-- ----------Baujaht
+insert into Baujahr(Baujahr)
+values("1990");  
+insert into Baujahr(Baujahr)
+values("1991");  
+insert into Baujahr(Baujahr)
+values("1992");  
+insert into Baujahr(Baujahr)
+values("1993");  
+insert into Baujahr(Baujahr)
+values("1994");  
+insert into Baujahr(Baujahr)
+values("1995");  
+insert into Baujahr(Baujahr)
+values("1996");  
+insert into Baujahr(Baujahr)
+values("1997");  
+insert into Baujahr(Baujahr)
+values("1998");  
+insert into Baujahr(Baujahr)
+values("1999");  
+insert into Baujahr(Baujahr)
+values("2000");
+insert into Baujahr(Baujahr)
+values("2001");  
+insert into Baujahr(Baujahr)
+values("2002");  
+insert into Baujahr(Baujahr)
+values("2003");  
+insert into Baujahr(Baujahr)
+values("2004");  
+insert into Baujahr(Baujahr)
+values("2005");  
+insert into Baujahr(Baujahr)
+values("2006");  
+insert into Baujahr(Baujahr)
+values("2007");  
+insert into Baujahr(Baujahr)
+values("2008");  
+insert into Baujahr(Baujahr)
+values("2009");  
+insert into Baujahr(Baujahr)
+values("2010");  
+insert into Baujahr(Baujahr)
+values("2011");  
+insert into Baujahr(Baujahr)
+values("2012");  
+insert into Baujahr(Baujahr)
+values("2013");  
+insert into Baujahr(Baujahr)
+values("2014");  
+insert into Baujahr(Baujahr)
+values("2015");  
+-- --------------------Leistung
+insert into Leistung(Leistung_in_ps)
+values("34"); 
+insert into Leistung(Leistung_in_ps)
+values("50"); 
+insert into Leistung(Leistung_in_ps)
+values("60"); 
+insert into Leistung(Leistung_in_ps)
+values("75"); 
+insert into Leistung(Leistung_in_ps)
+values("90"); 
+insert into Leistung(Leistung_in_ps)
+values("101"); 
+insert into Leistung(Leistung_in_ps)
+values("118"); 
+insert into Leistung(Leistung_in_ps)
+values("131"); 
+insert into Leistung(Leistung_in_ps)
+values("150"); 
+insert into Leistung(Leistung_in_ps)
+values("200"); 
+insert into Leistung(Leistung_in_ps)
+values("252");
+insert into Leistung(Leistung_in_ps)
+values("303");  
